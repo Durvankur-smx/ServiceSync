fb270c07fe98713cdeb2ad24d832d72f97d460f4
Durvankur Devikar <druv@Durvankurs-MacBook-Air.local>
2026-06-19 11:02:42 +0530

    Configure production API URL

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,11 +1,10 @@
-import axios from 'axios'
+import axios from "axios";
 
 const api = axios.create({
-  baseURL: 'http://localhost:8080',
-  headers: {
-    'Content-Type': 'application/json',
-  },
-})
+  baseURL: import.meta.env.VITE_API_URL,
+});
+
+export default api;
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('fixify_token')
